a46c067686c93fd059a9b3a9743dd8e06af37ccd
blonial <bloonial@gmail.com>
2020-10-24 14:22:33 +0200

    Configured redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
-import { createStore as createReduxStore } from 'redux';
+import { createStore as createReduxStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 
 import combinedReducers from './combinedReducers';
 
 function createStore() {
-  const store = createReduxStore(combinedReducers);
+  const store = createReduxStore(combinedReducers, applyMiddleware(thunk));
   const persistor = persistStore(store);
   return { store, persistor };
 }
