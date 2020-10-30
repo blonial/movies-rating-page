e405c35b4401b79feaaa2ba95ccaac18580272ef
blonial <bloonial@gmail.com>
2020-10-30 14:36:55 +0100

    Unstyled finish view

--- src/config/languages/finishView.languages.js
+++ src/config/languages/finishView.languages.js
@@ -1,11 +1,21 @@
 const data = {
   pl: {
-    title: 'Widok końcowy',
-    button: 'Resetuj',
+    thanks: 'Dziękujemy',
+    description:
+      'Dziękujemy bardzo że oceniłeś dla nas wybrane filmy! <br />' +
+      'Dzięki Tobie możemy dalej kontynuować pracę nad przedmiotem Uczenie Maszynowe! <br />' +
+      'Będziemy bardzo wdzięczni, jeśli poprosisz o ocenę również swoją rodzinę i znajomych. <br />' +
+      'Aby ocenić jeszcze raz - naciśnij poniższy przycisk!',
+    rateAgain: 'Oceń ponownie',
   },
   en: {
-    title: 'Finish view',
-    button: 'Reset',
+    thanks: 'Thanks ',
+    description:
+      'Thank you very much for rating the selected movies for us! <br />' +
+      'Thanks to you, we can continue working on the Machine Learning subject! <br />' +
+      'We would be very grateful if you would also ask your family and friends to rate movies too. <br />' +
+      'To rate again - press the button below! ',
+    rateAgain: 'Rate again',
   },
 };
 
--- src/views/FinishView/FinishView.js
+++ src/views/FinishView/FinishView.js
@@ -1,15 +1,18 @@
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { resetViewType } from '../../actions/viewType.actions';
 import { resetRatingMovie } from '../../actions/ratingMovie.actions';
 import { resetUser } from '../../actions/user.actions';
+import { getUserNick } from '../../selectors/user.selectors';
 
 import { useLanguage } from '../../hooks';
 
 function FinishView() {
   const dispatch = useDispatch();
   const language = useLanguage('finishView');
+
+  const nick = useSelector(getUserNick);
 
   const reset = useCallback(() => {
     resetViewType()(dispatch);
@@ -18,9 +21,15 @@ function FinishView() {
   }, [dispatch]);
 
   return (
-    <div>
-      {language.title}
-      <button onClick={reset}>{language.button}</button>
+    <div className='finish-view container d-flex flex-column justify-content-center'>
+      <h1 className='text-center'>
+        {language.thanks} {nick}!
+      </h1>
+      <span
+        className='text-center'
+        dangerouslySetInnerHTML={{ __html: language.description }}
+      />
+      <button onClick={reset}>{language.rateAgain}</button>
     </div>
   );
 }
